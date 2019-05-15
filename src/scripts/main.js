4427537ccb523cba088854a8ce9c193b9551099a
Carly Bergthold <carlybergthold@gmail.com>
2019-05-15 10:13:05 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,38 @@
+// // Lightning Exercise Mad Lib: Create a function that accepts the following object as an argument and returns a completed paragraph using the values of the object. Invoke the function and display the paragraph in the console.
+
+// const makeMadeLib = (obj) => {
+//     const wordArrays = Object.entries(obj)
+//     console.log(wordArrays)
+//     // console.log(`The ${noun} is ${verb-ing} with ${celebrity}`);
+// }
+
+// const madLibrary = {
+//    pluralNoun1: "bears",
+//    adjective1: "pretty",
+//    noun: "street",
+//    typeOfFood: "mexican",
+//    articleOfClothing: "dickey",
+//    verbEndingIning: "wallowing",
+//    pluralNoun2: "trees",
+//    pluralNoun3: "lasers",
+//    numberVal: "113",
+//    celebrity: "Taylor Swift",
+//    color: "golden rod",
+//    verbEndingIning2: "licking",
+//    typeOfFood: "Italian sweets",
+//    pluralNoun: "concrete shoes",
+//    adjective2: "gloomy",
+//    adjective3: "claustrophobic"
+// }
+
+// // function getKeyByValue(object, value) {
+// //     return Object.values(object).find(val => val[value] === key);
+// // }
+
+// // console.log(getKeyByValue(madLibrary, "noun"))
+
+// makeMadeLib(madLibrary);
+
+
+
+
